98f32ca51b2c357f2206203dab66b9e3b3303c6c
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-13 14:27:12 -0700

    background image set

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import PhotoOfDay from "./components/PhotoOfDay"
 
 const API_KEY = "DEMO_KEY"
 // const API_KEY = "MkDmR7vIsfFN3eSnARUsKJH4C0tPaXa8TcemitmE"
-//this would be a cute use of state that creates a button to switch between API key's, esp if the first has been timed out to show error screen.
+//this would be a cute use of state that creates a button to switch between API key's, esp if the first has been timed out to show error screen. Otherwise just build a mini logic thingy that checks if DEMO_KEY works and shuttles to the next if not.
 function App() {
   const [imgObj, setImgObj] = useState({})
   const [specificDate, setSpecificDate] = useState("&date=2012-03-14") //sample: &date=2012-03-14
-  //state for whether the 1st & 2nd menu are visible?? Dunno, the first CenterMenu will popup when the img is clicked on (containing "Back", "SideMenu" & "shuffle" buttons, maybe an "exitMenu" button). The second SideMenu will popup when it's button is pushed, replacing the CenterMenu (it'll add a dateSelector, AboutMe, & MoreInfo options). Hmm, that actually sounds kinda rough. Maybe we'll make the MoreInfo a toggle switch for the info. TBD.
+  //state for whether the 1st & 2nd menu are visible?? Dunnoyet, the first CenterMenu will popup when the img is clicked on (containing "Back", "SideMenu" & "shuffle" buttons, maybe an "exitMenu" button). The second SideMenu will popup when it's button is pushed, replacing the CenterMenu (it'll add a dateSelector, AboutMe, & MoreInfo options). Hmm, that actually sounds kinda rough. Maybe we'll make the MoreInfo a toggle switch for the info. TBD.
 
   useEffect(() => {
     Axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}${specificDate}`)
@@ -23,8 +23,10 @@ function App() {
         debugger
     })
   }, [])
-
-  console.log(imgObj)
+  console.log("timing:", imgObj)
+  let root = document.querySelector("body") //basis of setting background
+  // root.style.backgroundColor="black"; //
+  root.style.backgroundImage = `url(${imgObj.url})`
   return (
     <div className="App">
     <p>
